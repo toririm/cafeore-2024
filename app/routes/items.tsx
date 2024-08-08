c22330501ef9377501b77aca7137392608f62292
toririm <longtimedragon@icloud.com>
2024-08-09 02:55:09 +0900

    feat: form validation

--- app/routes/items.tsx
+++ app/routes/items.tsx
@@ -1,7 +1,10 @@
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
 import type { ActionFunction, MetaFunction } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import { Form, json, useActionData } from "@remix-run/react";
 import { addDoc, collection } from "firebase/firestore";
 import useSWRSubscription from "swr/subscription";
+import { z } from "zod";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
@@ -13,8 +16,27 @@ export const meta: MetaFunction = () => {
   return [{ title: "アイテム" }];
 };
 
+const schema = z.object({
+  name: z.string({ required_error: "名前が未入力です" }),
+  price: z.number({ required_error: "価格が未入力です" }),
+  type: z.enum(["hot", "ice", "ore", "milk"], {
+    // ここのenumは後で共通化したい
+    required_error: "種類が未選択です",
+    invalid_type_error: "不正な種類です",
+  }),
+});
+
 export default function Item() {
   const { data: items } = useSWRSubscription("items", collectionSub);
+  const lastResult = useActionData<typeof clientAction>();
+  const [form, fields] = useForm({
+    lastResult,
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema });
+    },
+    shouldValidate: "onBlur",
+    shouldRevalidate: "onInput",
+  });
 
   return (
     <div className="font-sans p-4">
@@ -28,27 +50,54 @@ export default function Item() {
           </li>
         ))}
       </ul>
-      <Form method="post">
-        <Input type="text" name="name" placeholder="名前" />
-        <Input type="number" name="price" placeholder="価格" />
-        <RadioGroup name="type">
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="hot" id="hot" />
-            <Label htmlFor="hot">ホット</Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="ice" id="ice" />
-            <Label htmlFor="ice">アイス</Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="ore" id="ore" />
-            <Label htmlFor="ore">オレ</Label>
-          </div>
-          <div className="flex items-center space-x-2">
-            <RadioGroupItem value="milk" id="milk" />
-            <Label htmlFor="milk">ミルク</Label>
-          </div>
-        </RadioGroup>
+      <Form method="post" id={form.id} onSubmit={form.onSubmit}>
+        <div>
+          <Input
+            type="text"
+            key={fields.name.key}
+            name={fields.name.name}
+            defaultValue={fields.name.initialValue}
+            required
+            placeholder="名前"
+          />
+          <span>{fields.name.errors}</span>
+        </div>
+        <div>
+          <Input
+            type="number"
+            key={fields.price.key}
+            name={fields.price.name}
+            defaultValue={fields.price.initialValue}
+            required
+            placeholder="価格"
+          />
+          <span>{fields.price.errors}</span>
+        </div>
+        <div>
+          <RadioGroup
+            key={fields.type.key}
+            name={fields.type.name}
+            defaultValue={fields.type.initialValue}
+          >
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="hot" id="hot" />
+              <Label htmlFor="hot">ホット</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="ice" id="ice" />
+              <Label htmlFor="ice">アイス</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="ore" id="ore" />
+              <Label htmlFor="ore">オレ</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="milk" id="milk" />
+              <Label htmlFor="milk">ミルク</Label>
+            </div>
+          </RadioGroup>
+          <span>{fields.type.errors}</span>
+        </div>
         <Button type="submit">登録</Button>
       </Form>
     </div>
@@ -57,14 +106,13 @@ export default function Item() {
 
 export const clientAction: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
-  const name = formData.get("name");
-  const price = formData.get("price");
-  const type = formData.get("type");
+  const submission = parseWithZod(formData, { schema });
 
-  // あとでマシなバリデーションにする e.g. zod, conform
-  if (!(name && price && type)) {
-    return new Response("Bad Request", { status: 400 });
+  if (submission.status !== "success") {
+    return json(submission.reply());
   }
+
+  const { name, price, type } = submission.value;
 
   // あとでマシなエラーハンドリングにする & 処理を別ファイルに切り分ける
   const docRef = await addDoc(collection(db, "items"), {
