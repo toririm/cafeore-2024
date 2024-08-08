dbbf1443f736cb63b3fd1296ea6c486571707173
toririm <longtimedragon@icloud.com>
2024-08-09 04:24:19 +0900

    feat: type-safe firestore

--- app/routes/items.tsx
+++ app/routes/items.tsx
@@ -4,35 +4,35 @@ import type { ActionFunction, MetaFunction } from "@remix-run/node";
 import { Form, json, useActionData } from "@remix-run/react";
 import { addDoc, collection } from "firebase/firestore";
 import useSWRSubscription from "swr/subscription";
-import { z } from "zod";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "~/components/ui/radio-group";
 import { db } from "~/firebase/firestore";
 import { collectionSub } from "~/firebase/subscription";
+import { itemSchema, itemtypes } from "~/models/item";
 
 export const meta: MetaFunction = () => {
   return [{ title: "アイテム" }];
 };
 
-const schema = z.object({
-  name: z.string({ required_error: "名前が未入力です" }),
-  price: z.number({ required_error: "価格が未入力です" }),
-  type: z.enum(["hot", "ice", "ore", "milk"], {
-    // ここのenumは後で共通化したい
-    required_error: "種類が未選択です",
-    invalid_type_error: "不正な種類です",
-  }),
-});
+const type2label = {
+  hot: "ホット",
+  ice: "アイス",
+  ore: "オレ",
+  milk: "ミルク",
+};
 
 export default function Item() {
-  const { data: items } = useSWRSubscription("items", collectionSub);
+  const { data: items } = useSWRSubscription(
+    "items",
+    collectionSub(itemSchema),
+  );
   const lastResult = useActionData<typeof clientAction>();
   const [form, fields] = useForm({
     lastResult,
     onValidate({ formData }) {
-      return parseWithZod(formData, { schema });
+      return parseWithZod(formData, { schema: itemSchema });
     },
     shouldValidate: "onBlur",
     shouldRevalidate: "onInput",
@@ -43,7 +43,7 @@ export default function Item() {
       <h1 className="text-3xl">アイテム</h1>
       <ul>
         {items?.map((item) => (
-          <li key={item.id}>
+          <li key={item.name}>
             <h2>{item.name}</h2>
             <p>{item.price}</p>
             <p>{item.type}</p>
@@ -79,22 +79,12 @@ export default function Item() {
             name={fields.type.name}
             defaultValue={fields.type.initialValue}
           >
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="hot" id="hot" />
-              <Label htmlFor="hot">ホット</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="ice" id="ice" />
-              <Label htmlFor="ice">アイス</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="ore" id="ore" />
-              <Label htmlFor="ore">オレ</Label>
-            </div>
-            <div className="flex items-center space-x-2">
-              <RadioGroupItem value="milk" id="milk" />
-              <Label htmlFor="milk">ミルク</Label>
-            </div>
+            {itemtypes.map((type) => (
+              <div key={type} className="flex items-center space-x-2">
+                <RadioGroupItem value={type} id={type} />
+                <Label htmlFor={type}>{type2label[type]}</Label>
+              </div>
+            ))}
           </RadioGroup>
           <span>{fields.type.errors}</span>
         </div>
@@ -106,7 +96,7 @@ export default function Item() {
 
 export const clientAction: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
-  const submission = parseWithZod(formData, { schema });
+  const submission = parseWithZod(formData, { schema: itemSchema });
 
   if (submission.status !== "success") {
     return json(submission.reply());
