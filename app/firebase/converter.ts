dc10bbda259bc363db4c422e70e7cd4296630988
toririm <longtimedragon@icloud.com>
2024-08-09 20:58:21 +0900

    feat: firestore Timestamp conveter

--- app/firebase/converter.ts
+++ app/firebase/converter.ts
@@ -3,6 +3,7 @@ import type {
   QueryDocumentSnapshot,
   SnapshotOptions,
 } from "firebase/firestore";
+import _ from "lodash";
 import type { ZodSchema } from "zod";
 
 export const converter = <T>(schema: ZodSchema<T>) => {
@@ -16,7 +17,32 @@ export const converter = <T>(schema: ZodSchema<T>) => {
     ) => {
       const data = snapshot.data(options);
       // id は Firestore のドキュメント ID を使う
-      return schema.parse({ ...data, id: snapshot.id });
+      const dataWithId = { ...data, id: snapshot.id };
+      const dateParsedData = parseDateProperty(dataWithId);
+      return schema.parse(dateParsedData);
     },
   };
+};
+
+// 通常の Firestore のデータは上記 Zod によってパースできるが
+// Firestore の Timestamp はパースできないため、個別でパースする
+
+// この関数の型注釈は若干嘘
+const parseDateProperty = (data: DocumentData): DocumentData => {
+  const parsedData = _.mapValues(data, (value) =>
+    // toDate が存在する場合は Timestamp 型としてパースする
+    value?.toDate ? value.toDate() : value,
+  );
+  const recursivelyParsedData = _.mapValues(parsedData, (value) => {
+    // 再帰的にパースする
+    switch (Object.prototype.toString.call(value)) {
+      case "[object Object]":
+        return parseDateProperty(value);
+      case "[object Array]":
+        return (value as Array<DocumentData>).map((v) => parseDateProperty(v));
+      default:
+        return value;
+    }
+  });
+  return recursivelyParsedData;
 };
