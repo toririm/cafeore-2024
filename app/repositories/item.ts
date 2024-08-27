1bb195d0f70ced82caea3b8e4f0e5cb9b6c431a7
lai_lai <aoichigo77@icloud.com>
2024-08-27 15:31:39 +0900

    add save

--- app/repositories/item.ts
+++ app/repositories/item.ts
@@ -1,28 +1,40 @@
-import { addDoc, collection, getDocs } from "firebase/firestore";
+import { addDoc, collection, doc, getDoc, getDocs, setDoc } from "firebase/firestore";
 import { converter } from "~/firebase/converter";
 import { db } from "~/firebase/firestore";
-import { Item, itemSchema } from "~/models/item";
+import { hasId } from "~/lib/typeguard";
+import { itemSchema } from "~/models/item";
 import { ItemRepository } from "./type";
 
 export const itemRepository: ItemRepository = {
+  save: async (item) => {
+    if (hasId(item)) {
+      const docRef = doc(db, "items", item.id).withConverter(
+        converter(itemSchema.required()),
+      );
+      await setDoc(docRef, item);
+      return item;
+    } else {
+      const colRef = collection(db, "items").withConverter(converter(itemSchema));
+      const docRef = await addDoc(colRef, item);
+      const resultDoc = await getDoc(
+        docRef.withConverter(converter(itemSchema.required())),
+      );
+      if (resultDoc.exists()) {
+        return resultDoc.data();
+      }
+      throw new Error("Failed to save item");
+    }
+  },
+  delete: async (id) => {},
+  findById: async (id: string) => {
+    // ここに Firestore からデータを取得する処理を記述
+    return null;
+  },
   findAll: async () => {
     const colRef = collection(db, "items").withConverter(
       converter(itemSchema.required()),
     );
     const docSnaps = await getDocs(colRef);
     return docSnaps.docs.map((doc) => doc.data());
-  },
-  findById: async (id: string) => {
-    // ここに Firestore からデータを取得する処理を記述
-    return null;
-  },
-  create: async (data: Item) => {
-    const docRef = await addDoc(collection(db, "items"), data);
-  },
-  update: async (data: Item) => {
-    // ここに Firestore のデータを更新する処理を記述
-  },
-  delete: async (id: string) => {
-    // ここに Firestore のデータを削除する処理を記述
   },
 };
