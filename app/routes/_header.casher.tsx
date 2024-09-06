209df744c431a49f7c4885a3aecf2058f1498d1f
Astalum <taaro20040526@icloud.com>
2024-09-07 08:49:33 +0900

    itemsへのキューの追加を別関数で実装

--- app/routes/_header.casher.tsx
+++ app/routes/_header.casher.tsx
@@ -12,6 +12,7 @@ import {
 } from "~/components/ui/alert-dialog";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
+import { Item } from "~/models/item";
 import { Order } from "~/models/order";
 
 const mockOrder: Order = {
@@ -37,76 +38,60 @@ const mockOrder: Order = {
 
 export default function Casher() {
   // const total = mockOrder.items.reduce((acc, cur) => acc + cur.price, 0);
-  const [recieved, setText] = useState(0);
-  const [items, setItems] = useState<
-    {
-      type: "hot" | "ice" | "ore" | "milk";
-      name: string;
-      price: number;
-      id?: string | undefined;
-    }[]
-  >([]);
-  const item_model = {
-    ore_blend: {
-      type: "ice" as "hot" | "ice" | "ore" | "milk",
-      name: "珈琲・俺ブレンド",
-      price: 300,
-    },
-    champ_blend: {
-      type: "hot" as "hot" | "ice" | "ore" | "milk",
-      name: "優勝ブレンド",
-      price: 300,
-    },
-    edel: {
-      type: "hot" as "hot" | "ice" | "ore" | "milk",
-      name: "エーデルワイス",
-      price: 300,
-    },
-    keny: {
-      type: "ice" as "hot" | "ice" | "ore" | "milk",
-      name: "ケニア",
-      price: 300,
-    },
-    bra: {
-      type: "hot" as "hot" | "ice" | "ore" | "milk",
-      name: "ブラジルブルボン",
-      price: 300,
-    },
-    ore: {
-      type: "ore" as "hot" | "ice" | "ore" | "milk",
-      name: "カフェオレ",
-      price: 300,
-    },
-    ice: {
-      type: "ice" as "hot" | "ice" | "ore" | "milk",
-      name: "アイスコーヒー",
-      price: 300,
-    },
-    milk: {
-      type: "milk" as "hot" | "ice" | "ore" | "milk",
-      name: "アイスミルク",
-      price: 300,
-    },
+  const ore_blend: Item = {
+    type: "ice",
+    name: "珈琲・俺ブレンド",
+    price: 300,
   };
+  const champ_blend: Item = {
+    type: "hot",
+    name: "優勝ブレンド",
+    price: 300,
+  };
+  const edel: Item = {
+    type: "hot",
+    name: "エーデルワイス",
+    price: 300,
+  };
+  const keny: Item = {
+    type: "ice",
+    name: "ケニア",
+    price: 300,
+  };
+  const bra: Item = {
+    type: "hot",
+    name: "ブラジルブルボン",
+    price: 300,
+  };
+  const ore: Item = {
+    type: "ore",
+    name: "カフェオレ",
+    price: 300,
+  };
+  const ice: Item = {
+    type: "ice",
+    name: "アイスコーヒー",
+    price: 300,
+  };
+  const milk: Item = {
+    type: "milk",
+    name: "アイスミルク",
+    price: 300,
+  };
+  const [recieved, setText] = useState(0);
+  const [order, setOrder] = useState(mockOrder);
 
-  console.log(mockOrder);
+  // console.log(mockOrder);
   return (
     <div>
       <div>
         <ul>
           <Button
             onClick={async () => {
-              mockOrder.items.push(item_model.ore_blend);
-              mockOrder.total = mockOrder.items.reduce(
-                (acc, cur) => acc + cur.price,
-                0,
-              );
-              setItems(mockOrder.items);
-              mockOrder.total = mockOrder.items.reduce(
-                (acc, cur) => acc + cur.price,
-                0,
-              );
+              add_order(ore_blend);
+              setOrder(mockOrder);
               console.log(mockOrder);
+              console.log(order);
             }}
           >
             珈琲・俺ブレンド
@@ -118,7 +103,7 @@ export default function Casher() {
           <li>
             <h2>合計金額：</h2>
             {/* <h3>{total}</h3> */}
-            <h3>{mockOrder.total}</h3>
+            <h3>{order.total}</h3>
           </li>
           <li>
             <h2>受領金額：</h2>
@@ -137,13 +122,9 @@ export default function Casher() {
                   <AlertDialogHeader>
                     <AlertDialogTitle>金額を確認してください</AlertDialogTitle>
                     <AlertDialogDescription>
-                      <p>{"受領額：" + recieved}</p>
-                      <p>{"合計金額：" + String(mockOrder.total)}</p>
-                      <p>
-                        {"お釣り：" +
-                          String(recieved - mockOrder.total) +
-                          " 円"}
-                      </p>
+                      <p>受領額： ¥${recieved}</p>
+                      <p>合計金額： ¥$(mockOrder.total)</p>
+                      <p>お釣り： ¥$(recieved - mockOrder.total) 円</p>
                     </AlertDialogDescription>
                   </AlertDialogHeader>
                   <AlertDialogFooter>
@@ -158,4 +139,11 @@ export default function Casher() {
       </div>
     </div>
   );
+}
+
+export function add_order(coffee: Item) {
+  mockOrder.items.push(coffee);
+  mockOrder.total = mockOrder.items.reduce((acc, cur) => acc + cur.price, 0);
+  console.log(mockOrder);
+  return mockOrder;
 }
