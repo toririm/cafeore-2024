4bf0247f221a61c754293373e36c8185b3ff3e8b
toririm <longtimedragon@icloud.com>
2024-09-07 15:19:13 +0900

    fix: typeguard only apply to id

--- app/lib/typeguard.ts
+++ app/lib/typeguard.ts
@@ -1,3 +1,5 @@
-export const hasId = <T>(obj: T): obj is Required<T> => {
+export const hasId = <T extends { id?: unknown }>(
+  obj: T,
+): obj is T & Record<"id", NonNullable<T["id"]>> => {
   return (obj as any).id !== undefined;
 };
