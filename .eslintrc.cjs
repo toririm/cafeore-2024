4949928dcf33186ea040561faaad20f56d3ef2af
toririm <longtimedragon@icloud.com>
2024-09-07 16:02:18 +0900

    fix: eslint ignore shadcn/ui

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,7 @@ module.exports = {
     commonjs: true,
     es6: true,
   },
-  ignorePatterns: ["!**/.server", "!**/.client"],
+  ignorePatterns: ["!**/.server", "!**/.client", "**/components/ui"],
   rules: {},
 
   // Base config
