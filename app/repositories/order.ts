e811f64eb43355e342b393e9a612ab088dca0081
toririm <longtimedragon@icloud.com>
2024-09-08 16:06:49 +0900

    change: use Entity in converter

--- app/repositories/order.ts
+++ app/repositories/order.ts
@@ -8,10 +8,10 @@ import {
   setDoc,
 } from "firebase/firestore";
 
-import { converter } from "~/firebase/converter";
+import { orderConverter } from "~/firebase/converter";
 import { db } from "~/firebase/firestore";
-import { hasId } from "~/lib/typeguard";
-import { orderSchema, type Order, type OrderWithId } from "~/models/order";
+import { hasId, type WithId } from "~/lib/typeguard";
+import { type OrderEntity } from "~/models/order";
 
 import { type OrderRepository } from "./type";
 
@@ -29,9 +29,7 @@ export const orderRepository: OrderRepository = {
   },
 
   findById: async (id) => {
-    const docRef = doc(db, "orders", id).withConverter(
-      converter(orderSchema.required()),
-    );
+    const docRef = doc(db, "orders", id).withConverter(orderConverter);
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
       return docSnap.data();
@@ -40,28 +38,24 @@ export const orderRepository: OrderRepository = {
   },
 
   findAll: async () => {
-    const colRef = collection(db, "orders").withConverter(
-      converter(orderSchema.required()),
-    );
+    const colRef = collection(db, "orders").withConverter(orderConverter);
     const docSnaps = await getDocs(colRef);
     return docSnaps.docs.map((doc) => doc.data());
   },
 };
 
-const update = async (order: OrderWithId): Promise<OrderWithId> => {
-  const docRef = doc(db, "orders", order.id).withConverter(
-    converter(orderSchema.required()),
-  );
+const update = async (
+  order: WithId<OrderEntity>,
+): Promise<WithId<OrderEntity>> => {
+  const docRef = doc(db, "orders", order.id).withConverter(orderConverter);
   await setDoc(docRef, order);
   return order;
 };
 
-const create = async (order: Order): Promise<OrderWithId> => {
-  const colRef = collection(db, "orders").withConverter(converter(orderSchema));
+const create = async (order: OrderEntity): Promise<WithId<OrderEntity>> => {
+  const colRef = collection(db, "orders").withConverter(orderConverter);
   const docRef = await addDoc(colRef, order);
-  const resultDoc = await getDoc(
-    docRef.withConverter(converter(orderSchema.required())),
-  );
+  const resultDoc = await getDoc(docRef.withConverter(orderConverter));
   if (resultDoc.exists()) {
     return resultDoc.data();
   }
