a5e4ae2f660082b290182ebd75a981a0872b14c2
Ryunosuke Tokinaga <59079411+toririm@users.noreply.github.com>
2024-09-10 01:40:53 +0900

    レジ画面v2を作ってみた (#83)
    - ref #4

---
+++ app/routes/cashier-v2.tsx
@@ -0,0 +1,107 @@
+import { parseWithZod } from "@conform-to/zod";
+import { useSubmit, type ClientActionFunction } from "@remix-run/react";
+import { useState } from "react";
+import useSWRSubscription from "swr/subscription";
+import { z } from "zod";
+
+import { Button } from "~/components/ui/button";
+import { itemConverter, orderConverter } from "~/firebase/converter";
+import { collectionSub } from "~/firebase/subscription";
+import { stringToJSONSchema } from "~/lib/custom-zod";
+import { type WithId } from "~/lib/typeguard";
+import { type2label, type ItemEntity } from "~/models/item";
+import { OrderEntity, orderSchema } from "~/models/order";
+import { orderRepository } from "~/repositories/order";
+
+export default function Cashier() {
+  const { data: items } = useSWRSubscription(
+    "items",
+    collectionSub(itemConverter),
+  );
+  const { data: orders } = useSWRSubscription(
+    "orders",
+    collectionSub(orderConverter),
+  );
+  const [orderItems, setOrderItems] = useState<WithId<ItemEntity>[]>([]);
+  const submit = useSubmit();
+
+  const nextOrderId =
+    (orders?.reduce((acc, cur) => Math.max(acc, cur.orderId), 0) ?? 1) + 1;
+  const newOrder = OrderEntity.createNew({ orderId: nextOrderId });
+  newOrder.items = orderItems;
+
+  const submitOrder = () => {
+    if (orderItems.length === 0) {
+      return;
+    }
+    submit(
+      { newOrder: JSON.stringify(newOrder.toOrder()) },
+      { method: "POST" },
+    );
+    setOrderItems([]);
+  };
+
+  return (
+    <>
+      <div className="grid grid-cols-2">
+        <div>
+          {items?.map((item) => (
+            <div key={item.id}>
+              <p>{item.name}</p>
+              <p>{item.price}</p>
+              <p>{type2label[item.type]}</p>
+              <Button
+                onClick={() => {
+                  setOrderItems([...orderItems, item]);
+                }}
+              >
+                追加
+              </Button>
+            </div>
+          ))}
+        </div>
+        <div>
+          <Button onClick={submitOrder}>提出</Button>
+          <Button onClick={() => setOrderItems([])}>クリア</Button>
+          <h1 className="text-lg">{`No. ${nextOrderId}`}</h1>
+          <div className="border-8">
+            <p>合計金額</p>
+            <p>{newOrder.total}</p>
+          </div>
+          {orderItems.map((item, idx) => (
+            <div key={`${idx}-${item.id}`}>
+              <p>{item.name}</p>
+              <p>{item.price}</p>
+              <p>{type2label[item.type]}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export const clientAction: ClientActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+
+  const schema = z.object({
+    newOrder: stringToJSONSchema.pipe(orderSchema),
+  });
+  const submission = parseWithZod(formData, {
+    schema,
+  });
+  if (submission.status !== "success") {
+    console.error(submission.error);
+    return submission.reply();
+  }
+
+  const { newOrder } = submission.value;
+  const order = OrderEntity.createNew({ orderId: newOrder.orderId });
+  order.items = newOrder.items;
+
+  const savedOrder = await orderRepository.save(order);
+
+  console.log("savedOrder", savedOrder);
+
+  return new Response("ok");
+};
