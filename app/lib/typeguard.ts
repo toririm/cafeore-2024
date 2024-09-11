eee4c1b72834faf537474d630aeed1de678279df
toririm <longtimedragon@icloud.com>
2024-09-11 13:12:35 +0900

    Revert "fix: `hasId`"
    This reverts commit 9be64e91c3707fef7983f391bfcdc892f19c0e1a.

--- app/lib/typeguard.ts
+++ app/lib/typeguard.ts
@@ -2,5 +2,5 @@ export type WithId<T extends { id?: unknown }> = T &
   Record<"id", NonNullable<T["id"]>>;
 
 export const hasId = <T extends { id?: unknown }>(obj: T): obj is WithId<T> => {
-  return obj.id !== undefined && obj.id !== null;
+  return obj.id !== undefined;
 };
