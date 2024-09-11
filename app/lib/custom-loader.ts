e28e9f8b1e57488dd5d6cdb8f435ce4fd3583754
toririm <longtimedragon@icloud.com>
2024-09-11 11:25:30 +0900

    chore: shoten

--- app/lib/custom-loader.ts
+++ app/lib/custom-loader.ts
@@ -1,5 +1,4 @@
 import { useLoaderData, type ClientLoaderFunction } from "@remix-run/react";
 
-export const useClientLoaderData = <T extends ClientLoaderFunction>(): Awaited<
-  ReturnType<T>
-> => useLoaderData<T>() as Awaited<ReturnType<T>>;
+export const useClientLoaderData = <T extends ClientLoaderFunction>() =>
+  useLoaderData<T>() as Awaited<ReturnType<T>>;
