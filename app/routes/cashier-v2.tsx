35516df8c60efdd1914dcd03b6f2b29d9ee95608
Ryunosuke Tokinaga <59079411+toririm@users.noreply.github.com>
2024-09-13 00:45:15 +0900

    ✨ キー操作でレジ画面を操作する (#84)
    close #16

--- app/routes/cashier-v2.tsx
+++ app/routes/cashier-v2.tsx
@@ -1,10 +1,11 @@
 import { parseWithZod } from "@conform-to/zod";
 import { useSubmit, type ClientActionFunction } from "@remix-run/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useSWRSubscription from "swr/subscription";
 import { z } from "zod";
 
 import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
 import { itemConverter, orderConverter } from "~/firebase/converter";
 import { collectionSub } from "~/firebase/subscription";
 import { stringToJSONSchema } from "~/lib/custom-zod";
@@ -12,6 +13,8 @@ import { type WithId } from "~/lib/typeguard";
 import { type2label, type ItemEntity } from "~/models/item";
 import { OrderEntity, orderSchema } from "~/models/order";
 import { orderRepository } from "~/repositories/order";
+
+const keys = ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";"];
 
 export default function Cashier() {
   const { data: items } = useSWRSubscription(
@@ -24,11 +27,16 @@ export default function Cashier() {
   );
   const [orderItems, setOrderItems] = useState<WithId<ItemEntity>[]>([]);
   const submit = useSubmit();
+  console.log("orders", orders);
 
-  const nextOrderId =
-    (orders?.reduce((acc, cur) => Math.max(acc, cur.orderId), 0) ?? 1) + 1;
+  const curOrderId =
+    orders?.reduce((acc, cur) => Math.max(acc, cur.orderId), 0) ?? 0;
+  const nextOrderId = curOrderId + 1;
   const newOrder = OrderEntity.createNew({ orderId: nextOrderId });
   newOrder.items = orderItems;
+  const [received, setReceived] = useState("");
+  const charge = Number(received) - newOrder.total;
+  const chargeView: string | number = charge < 0 ? "不足しています" : charge;
 
   const submitOrder = () => {
     if (orderItems.length === 0) {
@@ -39,7 +47,40 @@ export default function Cashier() {
       { method: "POST" },
     );
     setOrderItems([]);
+    setReceived("");
   };
+
+  useEffect(() => {
+    items?.forEach((item, idx) => {
+      const handler = (event: KeyboardEvent) => {
+        if (event.key === keys[idx]) {
+          setOrderItems((prevItems) => [...prevItems, item]);
+        }
+      };
+      window.addEventListener("keydown", handler);
+      return () => window.removeEventListener("keydown", handler);
+    });
+  }, [items]);
+
+  useEffect(() => {
+    const handler = (event: KeyboardEvent) => {
+      if (event.key === "Enter") {
+        submitOrder();
+      }
+    };
+    window.addEventListener("keydown", handler);
+    return () => window.removeEventListener("keydown", handler);
+  });
+
+  useEffect(() => {
+    const handler = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setOrderItems([]);
+      }
+    };
+    window.addEventListener("keydown", handler);
+    return () => window.removeEventListener("keydown", handler);
+  });
 
   return (
     <>
@@ -52,7 +93,7 @@ export default function Cashier() {
               <p>{type2label[item.type]}</p>
               <Button
                 onClick={() => {
-                  setOrderItems([...orderItems, item]);
+                  setOrderItems((prevItems) => [...prevItems, item]);
                 }}
               >
                 追加
@@ -61,6 +102,15 @@ export default function Cashier() {
           ))}
         </div>
         <div>
+          <p>操作</p>
+          <p>
+            お預かり金額入力欄にフォーカスを合わせたまま商品の追加やクリアができます
+          </p>
+          <ul>
+            <li>商品を追加: キーボードの a, s, d, f, g, h, j, k, l, ;</li>
+            <li>注文を提出: Enter</li>
+            <li>注文をクリア: Esc</li>
+          </ul>
           <Button onClick={submitOrder}>提出</Button>
           <Button onClick={() => setOrderItems([])}>クリア</Button>
           <h1 className="text-lg">{`No. ${nextOrderId}`}</h1>
@@ -68,11 +118,21 @@ export default function Cashier() {
             <p>合計金額</p>
             <p>{newOrder.total}</p>
           </div>
+          <Input
+            type="number"
+            value={received}
+            onChange={(e) => setReceived(e.target.value)}
+            placeholder="お預かり金額を入力"
+          />
+          <Input disabled value={chargeView} />
           {orderItems.map((item, idx) => (
-            <div key={`${idx}-${item.id}`}>
-              <p>{item.name}</p>
-              <p>{item.price}</p>
-              <p>{type2label[item.type]}</p>
+            <div key={`${idx}-${item.id}`} className="grid grid-cols-2">
+              <p className="font-bold text-lg">{idx + 1}</p>
+              <div>
+                <p>{item.name}</p>
+                <p>{item.price}</p>
+                <p>{type2label[item.type]}</p>
+              </div>
             </div>
           ))}
         </div>
