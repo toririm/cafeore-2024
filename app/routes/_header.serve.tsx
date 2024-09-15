ece11fab9fd55711d257110b77b973120664eea8
shoga <132179328+Yayoi2078@users.noreply.github.com>
2024-09-15 16:53:38 +0900

    オーダー情報のリアルタイム同期 (#99)
    close #40

--- app/routes/_header.serve.tsx
+++ app/routes/_header.serve.tsx
@@ -1,8 +1,10 @@
 import { type MetaFunction } from "@remix-run/react";
+import useSWRSubscription from "swr/subscription";
 
 import { Button } from "~/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
-import { useClientLoaderData } from "~/lib/custom-loader";
+import { orderConverter } from "~/firebase/converter";
+import { collectionSub } from "~/firebase/subscription";
 import { type2label } from "~/models/item";
 import { orderRepository } from "~/repositories/order";
 
@@ -16,7 +18,10 @@ export const clientLoader = async () => {
 };
 
 export default function Serve() {
-  const { orders } = useClientLoaderData<typeof clientLoader>();
+  const { data: orders } = useSWRSubscription(
+    "orders",
+    collectionSub(orderConverter),
+  );
 
   return (
     <div className="p-4 font-sans">
@@ -26,7 +31,7 @@ export default function Serve() {
       </div>
 
       <div className="grid grid-cols-4 gap-4">
-        {orders.map((order) => (
+        {orders?.map((order) => (
           <div key={order.id}>
             <Card>
               <CardHeader>
