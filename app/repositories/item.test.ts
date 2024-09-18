e1af08b96aa86d077e3b121346c299fda7607bce
Lai_lai <131964508+Lailai0477@users.noreply.github.com>
2024-09-18 11:43:36 +0900

    Issue/75 [test] itemRepository の単体テストを実装 (#105)
    close #75 
    close #60

---
+++ app/repositories/item.test.ts
@@ -0,0 +1,81 @@
+import { beforeAll, describe, expect, test } from "bun:test";
+
+import { initializeTestEnvironment } from "@firebase/rules-unit-testing";
+import { type Firestore } from "firebase/firestore";
+
+import firebasejson from "~/../firebase.json";
+import { type WithId } from "~/lib/typeguard";
+import { ItemEntity } from "~/models/item";
+
+import { itemRepoFactory } from "./item";
+import { type ItemRepository } from "./type";
+
+describe("[db] itemRepository", async () => {
+  // To use this environment, firebase emulator must be running.
+
+  let savedItemHoge: WithId<ItemEntity>;
+  let itemRepository: ItemRepository;
+
+  beforeAll(async () => {
+    const testEnv = await initializeTestEnvironment({
+      projectId: "demo-firestore",
+      firestore: {
+        host: "localhost",
+        port: firebasejson.emulators.firestore.port,
+      },
+    });
+    const testDB = testEnv
+      .unauthenticatedContext()
+      .firestore() as unknown as Firestore;
+    itemRepository = itemRepoFactory(testDB);
+  });
+
+  test("itemRepository is defined", () => {
+    expect(itemRepository).toBeDefined();
+  });
+
+  test("itemRepository.save (create)", async () => {
+    const item = ItemEntity.createNew({
+      name: "Hoge",
+      price: 100,
+      type: "hot",
+    });
+    savedItemHoge = await itemRepository.save(item);
+    expect(savedItemHoge.id).toBeDefined();
+  });
+
+  test("itemRepository.save (update)", async () => {
+    const savedItem = await itemRepository.save(savedItemHoge);
+    expect(savedItem.id).toEqual(savedItemHoge.id);
+  });
+
+  test("itemRepository.findById", async () => {
+    const item = ItemEntity.createNew({
+      name: "Fuga",
+      price: 200,
+      type: "ice",
+    });
+    const savedItem = await itemRepository.save(item);
+    const foundItem = await itemRepository.findById(savedItem.id);
+    expect(foundItem).toEqual(savedItem);
+  });
+
+  test("itemRepository.findAll", async () => {
+    const item = ItemEntity.createNew({ name: "Foo", price: 300, type: "ore" });
+    const savedItem = await itemRepository.save(item);
+    const items = await itemRepository.findAll();
+    expect(items).toContainEqual(savedItem);
+  });
+
+  test("itemRepository.delete", async () => {
+    const item = ItemEntity.createNew({
+      name: "Bar",
+      price: 400,
+      type: "milk",
+    });
+    const savedItem = await itemRepository.save(item);
+    await itemRepository.delete(savedItem.id);
+    const foundItem = await itemRepository.findById(savedItem.id);
+    expect(foundItem).toBeNull();
+  });
+});
