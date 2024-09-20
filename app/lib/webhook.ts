2badeac40ee7f68b8461fe47d6b7d6d0847726b8
Ryunosuke Tokinaga <59079411+toririm@users.noreply.github.com>
2024-09-21 02:55:08 +0900

    Slack通知基盤の作成 (#135)
    close #133

---
+++ app/lib/webhook.ts
@@ -0,0 +1,17 @@
+export const sendSlackMessage = async (message: string) => {
+  const webhookUrl = import.meta.env.VITE_WEBHOOK_URL;
+
+  if (!webhookUrl) {
+    throw new Error("WEBHOOK_URL is not defined");
+  }
+
+  const response = await fetch(webhookUrl, {
+    method: "POST",
+    body: JSON.stringify({ text: message }),
+  });
+
+  if (!response.ok) {
+    console.log(response);
+    throw new Error("Failed to send message");
+  }
+};
