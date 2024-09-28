c3d722a568bddcf0a1a3b699c0622c3b4f70907a
toririm <longtimedragon@icloud.com>
2024-09-28 20:56:31 +0900

    fix

--- app/components/ui/input-otp.tsx
+++ app/components/ui/input-otp.tsx
@@ -60,6 +60,8 @@ const InputOTPSeparator = React.forwardRef<
   React.ElementRef<"div">,
   React.ComponentPropsWithoutRef<"div">
 >(({ ...props }, ref) => (
+  // biome-ignore lint/a11y/useSemanticElements: <explanation>
+  // biome-ignore lint/a11y/useFocusableInteractive: <explanation>
   <div ref={ref} role="separator" {...props}>
     <Dot />
   </div>
