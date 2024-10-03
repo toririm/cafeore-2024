7c4ef3c8e927d68c557ff47d056f5b0d5f6625e7
toririm <longtimedragon@icloud.com>
2024-10-03 21:22:09 +0900

    inputStatus を端で止まるように

--- app/components/functional/useInputStatus.ts
+++ app/components/functional/useInputStatus.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 const InputStatusList = [
   "discount",
@@ -8,30 +8,33 @@ const InputStatusList = [
   "submit",
 ] as const;
 
+const inputLen = InputStatusList.length;
+
+const narrowInLen = (idx: number) => Math.min(Math.max(idx, 0), inputLen - 1);
+
 /**
  * CashierV2 のドメイン固有のフック
  *
  * 入力ステータスを管理する
  */
 const useInputStatus = () => {
-  const [inputStatus, setInputStatus] =
-    useState<(typeof InputStatusList)[number]>("discount");
+  const [idx, setIdx] = useState(0);
 
   const proceedStatus = useCallback(() => {
-    const idx = InputStatusList.indexOf(inputStatus);
-    setInputStatus(InputStatusList[(idx + 1) % InputStatusList.length]);
-  }, [inputStatus]);
+    setIdx((prev) => narrowInLen(prev + 1));
+  }, []);
 
   const previousStatus = useCallback(() => {
-    const idx = InputStatusList.indexOf(inputStatus);
-    setInputStatus(
-      InputStatusList[
-        (idx - 1 + InputStatusList.length) % InputStatusList.length
-      ],
-    );
-  }, [inputStatus]);
+    setIdx((prev) => narrowInLen(prev - 1));
+  }, []);
 
-  return { inputStatus, proceedStatus, previousStatus, setInputStatus };
+  const inputStatus = useMemo(() => InputStatusList[idx], [idx]);
+
+  const resetStatus = useCallback(() => {
+    setIdx(0);
+  }, []);
+
+  return { inputStatus, proceedStatus, previousStatus, resetStatus };
 };
 
 export { useInputStatus };
