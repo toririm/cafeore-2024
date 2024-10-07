602c2af64f663203a2e3c14139a616ec581e05e6
もにょっとしたぷにぷにのなにか <134484570+monyopuni@users.noreply.github.com>
2024-10-08 05:05:01 +0900

    オフラインの時にOutletが表示されないよう修正

--- pos/app/routes/_header.tsx
+++ pos/app/routes/_header.tsx
@@ -1,7 +1,24 @@
 import { Outlet } from "@remix-run/react";
-import { OnlineStatus } from "~/components/online-status";
+import { OnlineStatus,useOnlineStatus } from "~/components/online-status";
 
 export default function BaseHeader() {
+  const isOnline = useOnlineStatus();
+
+  if (!isOnline) {
+    return (
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold mb-4">⛔オフライン</h1>
+          <p className="text-xl">
+            インターネット接続が切断されています。
+            <br />
+            接続が回復するまでお待ちください。
+          </p>
+        </div>
+      </div>
+    );
+  }
+  
   return (
     <div>
       {/* TODO(toririm): デザインが微妙にダサいので何とかする。
