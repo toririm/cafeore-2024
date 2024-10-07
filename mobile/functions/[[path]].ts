389823f6f7396c86e40c89c9679ebb4e21841a2c
toririm <longtimedragon@icloud.com>
2024-10-07 16:51:50 +0900

    fix: biome & tsc

--- mobile/functions/[[path]].ts
+++ mobile/functions/[[path]].ts
@@ -1,6 +1,7 @@
+import type { ServerBuild } from "@remix-run/cloudflare";
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 import * as build from "../build/server";
 
 export const onRequest = createPagesFunctionHandler({
-  build: build,
+  build: build as unknown as ServerBuild,
 });
