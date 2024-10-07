969d82873541a683f38b3e9551b018cf5abb781e
lai_lai <aoichigo77@icloud.com>
2024-10-07 18:08:20 +0900

    chartをshadcnに変更

--- app/routes/_header.dashboard.tsx
+++ app/routes/_header.dashboard.tsx
@@ -1,10 +1,29 @@
 import type { MetaFunction } from "@remix-run/react";
 import dayjs from "dayjs";
 import { orderBy } from "firebase/firestore";
-import Highcharts from "highcharts";
-import HighchartsReact from "highcharts-react-official";
-import { useRef } from "react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Rectangle,
+  XAxis,
+  YAxis,
+} from "recharts";
 import useSWRSubscription from "swr/subscription";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "~/components/ui/chart";
+import type { ChartConfig } from "~/components/ui/chart";
+
 import {
   Table,
   TableBody,
@@ -23,7 +42,7 @@ export const meta: MetaFunction = () => {
   return [{ title: "注文状況" }];
 };
 
-const Dashboard = (props: HighchartsReact.Props) => {
+export default function Dashboard() {
   const { data: orders } = useSWRSubscription(
     "orders",
     collectionSub({ converter: orderConverter }, orderBy("orderId", "desc")),
@@ -38,58 +57,70 @@ const Dashboard = (props: HighchartsReact.Props) => {
     }
     return acc;
   }, 0);
-  const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
-  const itemNamesArray = items?.map((items) => items.id);
+  const itemNamesArray = items?.map((items) => items.name);
   const init = new Map<string, number>();
   const numPerItem = orders?.reduce((acc, cur) => {
     if (itemNamesArray !== undefined) {
       for (let i = 0; i < cur.items.length; i++) {
         for (let j = 0; j < itemNamesArray?.length; j++) {
-          if (cur.items[i].id === itemNamesArray[j]) {
-            const num = acc.get(cur.items[i].id) ?? 0;
-            acc.set(cur.items[i].id, num + 1);
+          if (cur.items[i].name === itemNamesArray[j]) {
+            const num = acc.get(cur.items[i].name) ?? 0;
+            acc.set(cur.items[i].name, num + 1);
           }
         }
       }
     }
     return acc;
   }, init);
-  const itemNames = (): string[] => {
-    const name: string[] = [];
-    if (numPerItem !== undefined && items !== undefined) {
-      const itemIds = [...numPerItem.keys()];
-      for (let i = 0; i < numPerItem.size; i++) {
-        for (let j = 0; j < items?.length; j++) {
-          if (itemIds[i] === items[j].id) {
-            name.push(items[j].name);
-          }
-        }
-      }
-    }
-    return name;
-  };
-  const itemValues = (): number[] => {
-    let values: number[] = [];
+  const itemValue = (name: string): number | undefined => {
+    console.log(name);
+    let valueNum = undefined;
     if (numPerItem !== undefined) {
-      values = [...numPerItem.values()];
+      valueNum = numPerItem.get(name);
     }
-    return values;
+    console.log(numPerItem);
+    return valueNum;
   };
-  const options: Highcharts.Options = {
-    title: {
-      text: "種類別杯数",
+  const chartData = [
+    {
+      name: "べっぴん",
+      num: itemValue("べっぴんブレンド"),
+      fill: "var(--color-hot)",
     },
-    series: [
-      {
-        type: "column",
-        data: itemValues(),
-      },
-    ],
-    xAxis: { categories: itemNames() },
-    yAxis: { type: "linear" },
-  };
-  console.log(itemValues());
-
+    {
+      name: "俺ブレ",
+      num: itemValue("珈琲・俺ブレンド"),
+      fill: "var(--color-hot)",
+    },
+    {
+      name: "ピンク",
+      num: itemValue("ピンクブルボン"),
+      fill: "var(--color-hot)",
+    },
+    {
+      name: "コスタ",
+      num: itemValue("コスタリカ レッドハニー"),
+      fill: "var(--color-hot)",
+    },
+    { name: "マンデ", num: itemValue("マンデリン"), fill: "var(--color-hot)" },
+    { name: "限定", num: itemValue("限定（仮）"), fill: "var(--color-hot)" },
+    { name: "氷", num: itemValue("アイスコーヒー"), fill: "var(--color-ice)" },
+    {
+      name: "Iceオレ",
+      num: itemValue("アイスオレ"),
+      fill: "var(--color-aulait)",
+    },
+    {
+      name: "Hotオレ",
+      num: itemValue("ホットオレ"),
+      fill: "var(--color-aulait)",
+    },
+    {
+      name: "ミルク",
+      num: itemValue("アイスミルク"),
+      fill: "var(--color-milk)",
+    },
+  ];
   return (
     <div className="p-4 font-sans">
       <div className="flex justify-between pb-4">
@@ -131,18 +162,52 @@ const Dashboard = (props: HighchartsReact.Props) => {
         </div>
         <div className="w-1/2">
           <div className="sticky top-0">
-            <HighchartsReact
-              highcharts={Highcharts}
-              options={options}
-              ref={chartComponentRef}
-              {...props}
-            />
+            <Card>
+              <CardHeader>
+                <CardTitle>商品ごとの杯数</CardTitle>
+                <CardDescription>{}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <ChartContainer config={chartConfig}>
+                  <BarChart accessibilityLayer data={chartData}>
+                    <CartesianGrid vertical={false} />
+                    <XAxis
+                      dataKey="name"
+                      tickLine={false}
+                      tickMargin={10}
+                      axisLine={false}
+                      tickFormatter={(value) => value.slice(0, 5)}
+                    />
+                    <YAxis />
+                    <ChartTooltip
+                      cursor={false}
+                      content={<ChartTooltipContent nameKey="name" />}
+                    />
+                    <Bar
+                      dataKey="num"
+                      radius={8}
+                      activeBar={({ ...props }) => {
+                        return (
+                          <Rectangle
+                            {...props}
+                            fillOpacity={0.8}
+                            stroke={props.payload.fill}
+                            strokeDasharray={4}
+                            strokeDashoffset={4}
+                          />
+                        );
+                      }}
+                    />
+                  </BarChart>
+                </ChartContainer>
+              </CardContent>
+            </Card>
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 const numOfCups = (order: OrderEntity): number => {
   return order.items.length;
@@ -155,4 +220,28 @@ const diffTime = (order: OrderEntity) => {
   );
 };
 
-export default Dashboard;
+const chartConfig = {
+  name: {
+    label: "杯数",
+  },
+  hot: {
+    label: "ホット",
+    color: "hsl(var(--chart-1))",
+  },
+  ice: {
+    label: "アイス",
+    color: "hsl(var(--chart-2))",
+  },
+  aulait: {
+    label: "オレ",
+    color: "hsl(var(--chart-3))",
+  },
+  milk: {
+    label: "ミルク",
+    color: "hsl(var(--chart-4))",
+  },
+  other: {
+    label: "Other",
+    color: "hsl(var(--chart-5))",
+  },
+} satisfies ChartConfig;
