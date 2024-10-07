8d07dd423b57dd488fe71c38c60f76510ca90d66
toririm <longtimedragon@icloud.com>
2024-10-07 16:43:18 +0900

    init

---
+++ mobile/functions/[[path]].ts
@@ -0,0 +1,6 @@
+import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
+import * as build from "../build/server";
+
+export const onRequest = createPagesFunctionHandler({
+  build: build,
+});
