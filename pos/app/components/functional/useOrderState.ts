9e447af3c9440970fe235a2ccbc1a4a1bb17a83c
toririm <longtimedragon@icloud.com>
2024-10-18 05:22:34 +0900

    chore: `""` -> `null` の処理を削除 Entity に任せる

--- pos/app/components/functional/useOrderState.ts
+++ pos/app/components/functional/useOrderState.ts
@@ -98,7 +98,7 @@ const setReceived: OrderReducer<SetReceived> = (state, action) => {
 
 const setDescription: OrderReducer<SetDescription> = (state, action) => {
   const updated = state.clone();
-  updated.description = action.description || null;
+  updated.description = action.description;
   return updated;
 };
 
