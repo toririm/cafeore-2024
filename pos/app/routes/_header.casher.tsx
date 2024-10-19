c7d5818f8997b6269a774a9fe012686ece605cc0
Astalum <131961897+Astalum@users.noreply.github.com>
2024-10-19 11:26:13 +0900

    指名：null問題 (#296)
    fix: #294

--- pos/app/routes/_header.casher.tsx
+++ pos/app/routes/_header.casher.tsx
@@ -237,7 +237,7 @@ export default function Casher() {
                             <SelectItem value="3rd">3rd</SelectItem>
                             <SelectItem value="fourth">4th</SelectItem>
                             <SelectItem value="fifth">5th</SelectItem>
-                            <SelectItem value="null">指名なし</SelectItem>
+                            <SelectItem value="">指名なし</SelectItem>
                           </SelectGroup>
                         </SelectContent>
                       </Select>
