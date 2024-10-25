962ec79d5544b367a866161e76bb84a2f4ecb3bb
Astalum <taaro20040526@icloud.com>
2024-10-26 02:34:14 +0900

    separate:files

--- pos/app/routes/casher-mini_s.tsx
+++ pos/app/routes/casher-mini_s.tsx
@@ -12,6 +12,19 @@ export default function CasherMini() {
   const curOrderId =
     orders?.reduce((acc, cur) => Math.max(acc, cur.orderId), 0) ?? 0;
   const nextOrderId = curOrderId + 1;
+  const order = orders ? orders[curOrderId] : undefined;
 
-  return <div>Next Order ID: {nextOrderId}</div>;
+  return (
+    <div className="flex h-full p-[20px]">
+      <div>No. {nextOrderId}</div>
+      <div>商品点数： {order ? order.items.length : 0} 点</div>
+      <div>小計： {order ? order.total : 0} 円</div>
+      <div>割引： {order ? order.discount : 0} 円</div>
+      <div>合計： {order ? order.billingAmount : 0} 円</div>
+      <div>お預り： {order ? order.received : 0} 円</div>
+      <div>
+        お釣り： {(order?.received ?? 0) - (order?.billingAmount ?? 0)} 円
+      </div>
+    </div>
+  );
 }
